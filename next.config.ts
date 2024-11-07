9941c333e9a3019b05cbf251747b23ed775b42ba
Onyinye Gold Udeozo <udeozoonyinye02@gmail.com>
2024-11-07 23:54:04 +0100

    modified the next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,24 @@ import path from "path"
 const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-    additionalData: `@use "variables.scss" as *; @use "mixins.scss" as *;`, // Import files here
   },
   /* other config options here */
-}
+  webpack: (config, options) => {
+    // Keep any existing webpack modifications
+    if (typeof nextConfig.webpack === "function") {
+      config = nextConfig.webpack(config, options)
+    }
 
-export default nextConfig
+    // Add our new resolution settings
+    config.resolve.extensions = [
+      ".tsx",
+      ".ts",
+      ".js",
+      ".jsx",
+      ...config.resolve.extensions,
+    ]
+    config.resolve.modules = ["node_modules", "./", ...config.resolve.modules]
+
+    return config
+  },
+}
